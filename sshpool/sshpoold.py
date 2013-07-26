8b2339a41f97d9a22f5dbb56ccf0836540706bd1
Abhinav Singh <mailsforabhinav@gmail.com>
2013-07-26 18:46:00 +0530

    sshpoold can accept both channel DSN and config file path

--- sshpool/sshpoold.py
+++ sshpool/sshpoold.py
@@ -8,23 +8,38 @@
     :copyright: (c) 2013 by Abhinav Singh.
     :license: BSD, see LICENSE for more details.
 """
+import sys
 import sshpool
 import argparse
+import logging
 
 from .channel import Channel
 from .rest import HTTP
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(message)s')
+logger = logging.getLogger('sshpoold')
 
 def main():
     parser = argparse.ArgumentParser(
         description='SSHPool daemon v%s' % sshpool.__version__,
         epilog='Having difficulty using SSHPool? Report at: %s/issues/new' % sshpool.__homepage__
     )
-    parser.add_argument('--channel', default=list(), action='append', help='alias://user:pass@host:port')
+    parser.add_argument('--channel', default=list(), action='append', help='Channel DSN e.g. alias://user:pass@host:port. Consult %s#channel-dsn for more details.' % sshpool.__homepage__)
+    parser.add_argument('--config', help='Config file path containing one channel per line')
     parser.add_argument('--host', default='127.0.0.1', help='SSHPool interface (default: 127.0.0.1)')
     parser.add_argument('--port', default=8877, type=int, help='SSHPool listening port (default: 8877)')
     args = parser.parse_args()
     
-    for channel in args.channel:
+    channels = args.channel
+    
+    if args.config:
+        try:
+            channels += open(args.config, 'rb').read().strip().split()
+        except IOError as e:
+            logger.error(e)
+            sys.exit(1)
+    
+    for channel in channels:
         Channel.init(channel)
     
     HTTP(args.host, args.port).start()
